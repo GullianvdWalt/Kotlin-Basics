e64584f689b5961bbcce58a573f04b9bb9b2e156
Gullian <gullianvdwalt1@gmail.com>
2021-05-02 11:50:30 +0200

    Lists

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,36 +1,51 @@
-// Global Variable
+// ### Global Variable ###
 var num2: Int = 2;
+// #### Array ####
 val programmingLanguages = arrayOf("Kotlin", "Java", "PHP", "C", "C++", "C#");
+// #### Lists ####
+val cars = listOf("BMW", "Volkswagen", "Nissan", "Mazda", "Porsche");
+
 // Unit is same as void, not needed to state
 fun getName(name: String, number : Int) = println("$name $number");
 // Arrays and Loops
-fun printLanguages(numNames: Int) {
-
-    if(programmingLanguages.size <= numNames){
-
+fun printLanguages(num: Int) {
+    println(programmingLanguages.size);
+    if(programmingLanguages.size >= num){
+        programmingLanguages.forEachIndexed{ index, programmingLanguage ->
+            if(index < num){
+                println("$index - $programmingLanguage");
+            }
+        }
     }else{
         println("Number is out of bounds!");
     }
 }
 fun printAllLanguages(){
-    // For Loop
+    // #### For Loop ####
     println("#### For Loop ###");
     for(programmingLanguage in programmingLanguages){
         println(programmingLanguage);
     }
     println("### For Each Loop ###");
-    // For Each Loop
+    // #### For Each Loop - default ####
     programmingLanguages.forEach{
         // it -> default name for each element
         println(it)
     }
+    // #### For Each Loop - Custom ####
+    programmingLanguages.forEach{programmingLanguage ->
+        // it -> default name for each element
+        println(programmingLanguage)
+    }
+    println("### For Loop Certain Amount (3) ###")
+    printLanguages(3);
 }
 
 
 fun main(args: Array<String>) {
-    // Variables
-    // Local variables
-    // Mutable variables (Can be reassigned)
+    // #### Variables ####
+    // ### Local variables ###
+    // ## Mutable variables (Can be reassigned) ##
     var num: Int = 1;
     // Readonly variables (Variable is final and cannot be changed
     val name: String = "Nate";
@@ -51,5 +66,12 @@ fun main(args: Array<String>) {
     getName("William",1);
     printAllLanguages();
 
+    programmingLanguages.forEachIndexed{ index, programmingLanguage ->
+        println("$index - $programmingLanguage");
+    }
 
+    println("### Loop List ###");
+    cars.forEach{ car ->
+        print(car + "\n");
+    }
 }
