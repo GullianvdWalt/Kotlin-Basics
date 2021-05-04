7f1363135a51bfdc5a150b0215e64e93426ba8e1
Gullian <gullianvdwalt1@gmail.com>
2021-05-04 08:05:47 +0200

    Interface overriding methods

--- src/main/kotlin/PersonInfoProvider.kt
+++ src/main/kotlin/PersonInfoProvider.kt
@@ -3,15 +3,19 @@
  * Created at 15:31 on May, 2021
  */
 interface PersonInfoProvider {
-    fun printInfo(person: Person)
+    val providerInfo : String;
+    fun printInfo(person: Person){
+        // Default interface methods
+        println("Info");
+        person.printPerson()
+    }
+
 }
 
 // Implement
 class BasicInfoProvider : PersonInfoProvider{
-
-    override fun printInfo(person: Person) {
-        println("Info");
-    }
+    override val providerInfo : String
+    get() = "BasicInfoProvider";
 }
 
 fun main(){
