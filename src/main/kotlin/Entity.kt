514bebf831b6950a1c0f52679ac324c2ee42f112
Gullian <gullianvdwalt1@gmail.com>
2021-05-05 13:30:35 +0200

    Enum, when block

--- src/main/kotlin/Entity.kt
+++ src/main/kotlin/Entity.kt
@@ -1,10 +1,31 @@
+import java.util.*
+
 /**
  * @author Gullian Van Der Walt
  * Created at 13:19 on May, 2021
  */
+// Enum
+enum class EntityType{
+    EASY,
+    MEDIUM,
+    HARD
+}
+
 // Singleton
 object Factory {
-    fun create() = Entity("id", "John");
+    fun create(type: EntityType) : Entity {
+        val id = UUID.randomUUID().toString();
+        /*
+            Kotlin when:
+            advance form of switch
+         */
+        val name = when (type){
+            EntityType.EASY -> "Easy"
+            EntityType.MEDIUM -> "Medium"
+            EntityType.HARD -> "Hard"
+        }
+        return Entity(id, name);
+    }
 }
 class Entity (val id: String, val name : String){
     override fun toString(): String {
@@ -13,6 +34,6 @@ class Entity (val id: String, val name : String){
 }
 
 fun main(){
-    val entity = Factory.create();
+    val entity = Factory.create(EntityType.EASY);
     println(entity);
 }
